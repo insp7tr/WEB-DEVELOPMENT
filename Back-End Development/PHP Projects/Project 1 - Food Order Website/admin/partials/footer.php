d969d4188907804e52ba4835d892aa6b44620566
insp7tr <91053836+insp7tr@users.noreply.github.com>
2022-07-24 14:46:07 +0200

    Delete Back-End Development directory

--- Back-End Development/PHP Projects/Project 1 - Food Order Website/admin/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!--Footer Section Starts here-->
-<section class="footer">
-    <div class="wrapper">
-        <p class="text-center">2020 All Rights Reserved by BSFS. Developed by - <a href="">M.T. Patel</a></p>
-    </div>      
-</section>
-<!--Footer Section Ends here-->
-</body>
-</html>
